b43bf023f4d295491a032c9e5167cc38100e5e4f
Mittal <mittalkadia2021@gmail.com>
2021-05-15 16:44:20 +0100

    Changes done

--- src/test/java/org/example/calculater.java
+++ src/test/java/org/example/calculater.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import org.junit.Test;
+
 public class calculater {
     public static void main(){
         System.out.println("Mittal");
+        System.out.println("Riya");
     }
-
 }
